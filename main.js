9c8d4fe383e058ff87ae52e7de6db3458fdc11f6
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-10 18:51:15 +0100

    created mouseover function on the stars rating icons

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const start = document.querySelectorAll('fa-star-o')
+const selectedRatingValueText = document.querySelector('.selected-rating-value');
+
+let currentTotalSelectedStar = -1;
+
+start.forEach((startItem, index) => {
+    startItem.dataset.rating = index +1;
+    startItem.addEventListener('mouseover', handleMouseOver);
+    startItem.addEventListener('click', handleOnClick);
+    startItem.addEventListener('mouseleave', handleMouseLeave);
+})
+
+function handleMouseOver(event){
+    console.log(event.target.dataset);
+}
+
+handleMouseOver();
